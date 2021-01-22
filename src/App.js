b1c23e4c9042d63acea3109bd15b399f0c2f25a6
Matheus <mmathias2011@outlook.com>
2021-01-22 12:37:16 -0300

    Adição de alguns arquivos svg e estilização basica

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Login from "./Components/Login/Login";
+import Home from "./Home";
+import './App.css'
 
 function App() {
   return (
     <div>
-      <h1>Teste</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={ <Home /> }></Route>
+          <Route path="login" element={ <Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Header = () => {  
+    return (
+        <div>
+            <nav className="container">
+                <Link to="/"> Home</Link>
+                <Link to="login"> Login / Criar Conta</Link>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
