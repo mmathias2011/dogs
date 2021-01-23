93b4abf373f5721fc972bebaaf264b6142964329
Matheus <mmathias2011@outlook.com>
2021-01-23 16:16:21 -0300

    Estilização do Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import style from './Header.module.css'
+import { ReactComponent as Dogs } from '../Assets/dogs.svg'
 
 
 const Header = () => {  
     return (
-        <div>
-            <nav className="container">
-                <Link to="/"> Home</Link>
-                <Link to="login"> Login / Criar Conta</Link>
+        <header className={style.header}>
+            <nav className={`${style.nav} container`}>
+                <Link className={style.logo} to="/" aria-label="Dogs - Home"> 
+                <Dogs />
+                </Link>
+                <Link className={style.login} to="login"> Login / Criar Conta</Link>
             </nav>
-        </div>
+        </header>
     )
 }
 
